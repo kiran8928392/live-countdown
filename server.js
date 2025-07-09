dc9d0621dbfced66a3d52d557f479d5d1ea71139
kiran8928392 <kirancrochet@gmail.com>
2025-07-09 20:05:31 +0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+{
+  "name": "countdown-stream",
+  "version": "1.0.0",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "ffmpeg-static": "^4.4.0"
+  }
+}
